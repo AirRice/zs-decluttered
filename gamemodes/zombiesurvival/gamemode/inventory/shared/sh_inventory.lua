2bc4b785c560e794f83f13cf4644de61baa9b8ab
AirRice <jpumph2@gmail.com>
2025-08-14 18:37:12 +0100

    Clientside Frail debuff changes

--- gamemodes/zombiesurvival/gamemode/inventory/shared/sh_inventory.lua
+++ gamemodes/zombiesurvival/gamemode/inventory/shared/sh_inventory.lua
@@ -631,6 +631,9 @@ GM:AddSkillModifier(trinket, SKILLMOD_SCRAP_START, 1)
 
 trinket = GM:AddDebuffTrinket("DEBUFF: Frail", "d_insured", "+5 starting points\nCannot heal by any means beyond 25% maximum health")
 GM:AddSkillModifier(trinket, SKILLMOD_POINTS, 5)
+GM:AddSkillFunction(trinket, function(pl, active)
+	pl:SetDTBool(DT_PLAYER_BOOL_FRAIL, active)
+end)
 
 trinket = GM:AddDebuffTrinket("DEBUFF: Oversized Harnesses", "d_overharness", "-5% resupply delay\nFor the first 6 seconds after initiating barricade phasing, you cannot move")
 GM:AddSkillModifier(trinket, SKILLMOD_RESUPPLY_DELAY_MUL, -0.05)
